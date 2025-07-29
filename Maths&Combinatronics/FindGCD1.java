a371c3136a2b449df2ab63d30f94b06bedcdd952
ghost <ahirtech.ap@gmail.com>
2025-07-29 21:37:09 +0530

    Approach 1 to find GCD

---
+++ Maths&Combinatronics/FindGCD1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class FindGCD1 {
+    static int gcd(int a, int b) {
+        int result = Math.min(a,b);
+
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        int a = 20, b = 28;
+        System.out.println(gcd(a,b));
+    }
+}
